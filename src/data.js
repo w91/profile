b3f6298100d47ad6554ca7dc1c77bdccc908df2b
niunai <niunai@youzan.com>
2019-03-09 20:23:57 +0800

    feat: 添加作品图片

--- src/data.js
+++ src/data.js
@@ -8,11 +8,56 @@ import c4d7 from './assets/images/c4d/C4D-7.jpg';
 import c4d8 from './assets/images/c4d/C4D-8.jpg';
 import c4d9 from './assets/images/c4d/C4D-9.jpg';
 
+import shouhui1 from './assets/images/shouhui/shouhui-1.jpg';
+import shouhui2 from './assets/images/shouhui/shouhui-2.jpg';
+import shouhui3 from './assets/images/shouhui/shouhui-3.jpg';
+import shouhui4 from './assets/images/shouhui/shouhui-4.jpg';
+import shouhui5 from './assets/images/shouhui/shouhui-5.jpg';
+import shouhui6 from './assets/images/shouhui/shouhui-6.jpg';
+
+import bote1 from './assets/images/bote/bote-1.jpg';
+import bote2 from './assets/images/bote/bote-2.jpg';
+import bote3 from './assets/images/bote/bote-3.jpg';
+import bote4 from './assets/images/bote/bote-4.jpg';
+import bote5 from './assets/images/bote/bote-5.jpg';
+import bote6 from './assets/images/bote/bote-6.jpg';
+import bote7 from './assets/images/bote/bote-7.jpg';
+import bote8 from './assets/images/bote/bote-8.jpg';
+
+import jijin1 from './assets/images/jijin/jijin-1.jpg';
+import jijin2 from './assets/images/jijin/jijin-2.jpg';
+import jijin3 from './assets/images/jijin/jijin-3.jpg';
+import jijin4 from './assets/images/jijin/jijin-4.jpg';
+import jijin5 from './assets/images/jijin/jijin-5.jpg';
+import jijin6 from './assets/images/jijin/jijin-6.jpg';
+import jijin62 from './assets/images/jijin/jijin-6-2.jpg';
+import jijin7 from './assets/images/jijin/jijin-7.jpg';
+import jijin8 from './assets/images/jijin/jijin-8.jpg';
+import jijin9 from './assets/images/jijin/jijin-9.jpg';
+
 export const WORKS_MAP = {
   c4d: {
     width: 900,
     photos: [
       c4d1, c4d2, c4d3, c4d4, c4d5, c4d6, c4d7, c4d8, c4d9
+    ]
+  },
+  shouhui: {
+    width: 1280,
+    photos: [
+      shouhui1, shouhui2, shouhui3, shouhui4, shouhui5, shouhui6
+    ]
+  },
+  bote: {
+    width: 1280,
+    photos: [
+      bote1, bote2, bote3, bote4, bote5, bote6, bote7, bote8
+    ]
+  },
+  jijin: {
+    width: 1280,
+    photos: [
+      jijin1, jijin2, jijin3, jijin4, jijin5, jijin6, jijin62, jijin7, jijin8, jijin9
     ]
   }
 };
