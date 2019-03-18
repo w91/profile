b3f3f1b7a514c826911ca82cd2d5d01d244f9086
niunai <niunai@youzan.com>
2019-03-18 23:47:58 +0800

    使用原图

--- src/pages/about/index.js
+++ src/pages/about/index.js
@@ -1,16 +1,19 @@
 import React, { Component } from 'react';
-import PageTitle from '../../components/page-title'
-import resume from '../../assets/images/resume.jpg';
+import PageTitle from '../../components/page-title';
 import './index.scss';
 
 class About extends Component {
   render() {
     return (
       <div className="about-me">
-        <div className="divider"></div>
+        <div className="divider" />
         <PageTitle main="ABOUT ME" sub="关于我的" />
-        <img className="about-me__resume" src={resume} alt="resume" />
-        <div className="about-me-empty"></div>
+        <img
+          className="about-me__resume"
+          src="http://designs.test.upcdn.net/images/resume.jpg"
+          alt="resume"
+        />
+        <div className="about-me-empty" />
       </div>
     );
   }
