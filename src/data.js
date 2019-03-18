99da07a4967cb994df5fc062d98a638c6bb37ddb
niunai <niunai@youzan.com>
2019-03-18 22:59:26 +0800

    补充案例图片

--- src/data.js
+++ src/data.js
@@ -123,4 +123,103 @@ export const WORKS_MAP = {
       'http://designs.test.upcdn.net/images/guifan/guifan-28.jpg'
     ]
   },
+  jianwai: {
+    width: 1280,
+    photos: [
+      'http://designs.test.upcdn.net/images/jianwai/jianwai-1.jpg',
+      'http://designs.test.upcdn.net/images/jianwai/jianwai-2.jpg',
+      'http://designs.test.upcdn.net/images/jianwai/jianwai-3.jpg',
+      'http://designs.test.upcdn.net/images/jianwai/jianwai-4.jpg',
+      'http://designs.test.upcdn.net/images/jianwai/jianwai-5.jpg',
+      'http://designs.test.upcdn.net/images/jianwai/jianwai-6.jpg',
+      'http://designs.test.upcdn.net/images/jianwai/jianwai-7.jpg',
+      'http://designs.test.upcdn.net/images/jianwai/jianwai-8.jpg',
+      'http://designs.test.upcdn.net/images/jianwai/jianwai-9.jpg',
+      'http://designs.test.upcdn.net/images/jianwai/jianwai-10.jpg',
+      'http://designs.test.upcdn.net/images/jianwai/jianwai-11.jpg',
+      'http://designs.test.upcdn.net/images/jianwai/jianwai-12.jpg',
+      'http://designs.test.upcdn.net/images/jianwai/jianwai-13.jpg',
+      'http://designs.test.upcdn.net/images/jianwai/jianwai-14.jpg',
+      'http://designs.test.upcdn.net/images/jianwai/jianwai-15.jpg',
+      'http://designs.test.upcdn.net/images/jianwai/jianwai-16.jpg'
+    ]
+  },
+  zhigeng: {
+    width: 1280,
+    photos: [
+      'http://designs.test.upcdn.net/images/zhigeng/zhigeng-1.jpg',
+      'http://designs.test.upcdn.net/images/zhigeng/zhigeng-2.jpg',
+      'http://designs.test.upcdn.net/images/zhigeng/zhigeng-3.jpg',
+      'http://designs.test.upcdn.net/images/zhigeng/zhigeng-4.jpg',
+      'http://designs.test.upcdn.net/images/zhigeng/zhigeng-5.jpg',
+      'http://designs.test.upcdn.net/images/zhigeng/zhigeng-6.jpg',
+      'http://designs.test.upcdn.net/images/zhigeng/zhigeng-7.jpg',
+      'http://designs.test.upcdn.net/images/zhigeng/zhigeng-8.jpg',
+      'http://designs.test.upcdn.net/images/zhigeng/zhigeng-9.jpg',
+      'http://designs.test.upcdn.net/images/zhigeng/zhigeng-10.jpg',
+      'http://designs.test.upcdn.net/images/zhigeng/zhigeng-11.jpg',
+      'http://designs.test.upcdn.net/images/zhigeng/zhigeng-12.jpg',
+      'http://designs.test.upcdn.net/images/zhigeng/zhigeng-13.jpg',
+      'http://designs.test.upcdn.net/images/zhigeng/zhigeng-14.jpg',
+      'http://designs.test.upcdn.net/images/zhigeng/zhigeng-15.jpg',
+      'http://designs.test.upcdn.net/images/zhigeng/zhigeng-16.jpg',
+      'http://designs.test.upcdn.net/images/zhigeng/zhigeng-17.jpg',
+      'http://designs.test.upcdn.net/images/zhigeng/zhigeng-18.jpg',
+      'http://designs.test.upcdn.net/images/zhigeng/zhigeng-19.jpg',
+      'http://designs.test.upcdn.net/images/zhigeng/zhigeng-20.jpg',
+      'http://designs.test.upcdn.net/images/zhigeng/zhigeng-21.jpg',
+      'http://designs.test.upcdn.net/images/zhigeng/zhigeng-22.jpg',
+      'http://designs.test.upcdn.net/images/zhigeng/zhigeng-23.jpg',
+      'http://designs.test.upcdn.net/images/zhigeng/zhigeng-24.jpg',
+      'http://designs.test.upcdn.net/images/zhigeng/zhigeng-25.jpg',
+      'http://designs.test.upcdn.net/images/zhigeng/zhigeng-26.jpg',
+      'http://designs.test.upcdn.net/images/zhigeng/zhigeng-27.jpg'
+    ]
+  },
+  ai: {
+    width: 1280,
+    photos: [
+      'http://designs.test.upcdn.net/images/ai/ai-1.jpg',
+      'http://designs.test.upcdn.net/images/ai/ai-2.jpg',
+      'http://designs.test.upcdn.net/images/ai/ai-3.jpg',
+      'http://designs.test.upcdn.net/images/ai/ai-4.jpg',
+      'http://designs.test.upcdn.net/images/ai/ai-5.jpg',
+      'http://designs.test.upcdn.net/images/ai/ai-6.jpg',
+      'http://designs.test.upcdn.net/images/ai/ai-7.jpg',
+      'http://designs.test.upcdn.net/images/ai/ai-8.jpg',
+      'http://designs.test.upcdn.net/images/ai/ai-9.jpg',
+      'http://designs.test.upcdn.net/images/ai/ai-10.jpg',
+      'http://designs.test.upcdn.net/images/ai/ai-11.jpg',
+      'http://designs.test.upcdn.net/images/ai/ai-12.jpg',
+      'http://designs.test.upcdn.net/images/ai/ai-13.jpg',
+      'http://designs.test.upcdn.net/images/ai/ai-14.jpg',
+      'http://designs.test.upcdn.net/images/ai/ai-15.jpg',
+      'http://designs.test.upcdn.net/images/ai/ai-16.jpg',
+      'http://designs.test.upcdn.net/images/ai/ai-17.jpg',
+      'http://designs.test.upcdn.net/images/ai/ai-18.jpg',
+      'http://designs.test.upcdn.net/images/ai/ai-19.jpg',
+      'http://designs.test.upcdn.net/images/ai/ai-20.jpg',
+      'http://designs.test.upcdn.net/images/ai/ai-21.jpg',
+      'http://designs.test.upcdn.net/images/ai/ai-22.jpg'
+    ]
+  },
+  dd: {
+    width: 1280,
+    photos: [
+      'http://designs.test.upcdn.net/images/dd/dd-1.jpg',
+      'http://designs.test.upcdn.net/images/dd/dd-2.jpg',
+      'http://designs.test.upcdn.net/images/dd/dd-3.jpg',
+      'http://designs.test.upcdn.net/images/dd/dd-4.jpg',
+      'http://designs.test.upcdn.net/images/dd/dd-5.jpg',
+      'http://designs.test.upcdn.net/images/dd/dd-6.jpg',
+      'http://designs.test.upcdn.net/images/dd/dd-7.jpg',
+      'http://designs.test.upcdn.net/images/dd/dd-8.jpg',
+      'http://designs.test.upcdn.net/images/dd/dd-9.jpg',
+      'http://designs.test.upcdn.net/images/dd/dd-10.jpg',
+      'http://designs.test.upcdn.net/images/dd/dd-11.jpg',
+      'http://designs.test.upcdn.net/images/dd/dd-12.jpg',
+      'http://designs.test.upcdn.net/images/dd/dd-13.jpg',
+      'http://designs.test.upcdn.net/images/dd/dd-14.jpg'
+    ]
+  }
 };
